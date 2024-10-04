b27e112c1234601eb2e074c6b703e41c5fc40ce3
Osmar Mendes <99884631+osmarmcn@users.noreply.github.com>
2024-10-03 22:35:44 -0300

    Add files via upload

---
+++ js/cotacaoMoedas.js
@@ -0,0 +1,46 @@
+async function converterMoeda(codigoMoeda) {
+    let resposta = await fetch(`https://economia.awesomeapi.com.br/last/${codigoMoeda}-BRL`)
+
+    let cotacao = await resposta.json()
+
+    console.log( await cotacao)
+
+    montarTela( await cotacao[codigoMoeda + 'BRL'])
+}
+
+function montarTela(objeto){
+
+    let conversaoMoedaDiv = document.getElementById('conversaoMoeda')
+
+    conversaoMoedaDiv.innerHTML = ''
+
+    let titulo = document.createElement('h1')
+    titulo.innerHTML = objeto.name
+
+    let valor = document.createElement('p')
+    let valorOriginalReal = document.getElementById('valor').value
+    valor.innerHTML = `R$: ${valorOriginalReal}`
+
+    let valorConvertido = document.createElement('p');
+    valorConvertido.innerHTML = `${objeto.code}: ${(valorOriginalReal / objeto.bid).toFixed(2)}`
+
+    conversaoMoedaDiv.appendChild(titulo)
+    conversaoMoedaDiv.appendChild(valor)
+    conversaoMoedaDiv.appendChild(valorConvertido)
+
+    let dataCotacao = document.createElement('h5')
+    dataCotacao.innerHTML = objeto.create_date
+    conversaoMoedaDiv.appendChild(dataCotacao)
+}
+
+let btnUsd = document.getElementById('usd')
+btnUsd.addEventListener('click', ()=>{converterMoeda('USD')})
+
+let btnEur = document.getElementById('eur')
+btnEur.addEventListener('click', ()=>{converterMoeda('EUR')})
+
+let btnBtc = document.getElementById('btc')
+btnBtc.addEventListener('click', ()=>{converterMoeda('BTC')})
+
+let btnLibra = document.getElementById('gbp')
+btnLibra.addEventListener('click', ()=>{converterMoeda('GBP')})
